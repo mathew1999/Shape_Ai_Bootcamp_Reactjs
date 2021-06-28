f19854eced7d1020653966e46928b2c0dcd2f7d5
mathew1999 <44080257+mathew1999@users.noreply.github.com>
2021-06-28 12:09:14 +0530

    Add files via upload

---
+++ Heading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Heading()
+{
+  return(
+    <h1 className="hello">Hello</h1>
+  );
+}
+
+export default Heading;
+
---
+++ Info.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Info()
+{
+  return(
+  <div className="note">
+   <h1 >Javascript and React.js</h1>
+   <p >A Basic web dev React.js Bootcamp</p>
+  </div>
+  );
+}
+
+export default Info;
